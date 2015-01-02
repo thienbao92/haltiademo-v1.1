aed91c58593b7ba95491006a6bdab4c7d51c298e
Vu Dao <anhvudao17@gmail.com>
2015-01-02 10:17:36 +0200

    change Events 1st item, organize events

--- www/js/controllers/controllers.js
+++ www/js/controllers/controllers.js
@@ -16,41 +16,6 @@ angular.module('starter.controllers', [])
   $scope.events = Events;
 })
 
-.factory('News', function() {
-  var News = [
-    { title: 'Reggae',
-     thumbnail:'img/ionic.png',
-     date: '1.11.14',
-     content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-     id: 1 },
-    { title: 'Chill',
-     thumbnail:'img/ionic.png',
-     date: '2.11.14',
-     content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-     id: 2 },
-    { title: 'Dubstep',
-     thumbnail:'img/ionic.png',
-     date: '3.11.14',
-     content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', id: 3 },
-    { title: 'Haltia´s first special exhibition now at Helsinki-Vantaa airport',
-     thumbnail:'img/ionic.png',
-     date: '10.04.2014',
-     content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-     id: 4 },
-    { title: 'Haltia has rapidly become a popular destination',
-     thumbnail:'img/ionic.png',
-     date: '09.06.2014',
-     content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-     id: 5 },
-    { title: 'Kayaking and canoeing in nuuksio',
-     thumbnail:'img/ionic.png',
-     date: '28.06.2014',
-     content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-     id: 6 }
-  ];
-  return News;
-})
-
 .controller('NewsCtrl', function($scope, News) {
   $scope.news = News;
 })
